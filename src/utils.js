04ca98e2c61a3be9cd407e4155272b18e022af09
Jorin Vogel <hi@jorin.me>
2018-12-01 12:57:10 +0100

    Fix vulnerable deps and build latest version

--- src/utils.js
+++ src/utils.js
@@ -18,7 +18,7 @@ setModes()
 // For more info:
 // http://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript/3561711#3561711
 utils.escapeRegExp = function (str) {
-  return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+  return str.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
 }
 
 // in seconds
